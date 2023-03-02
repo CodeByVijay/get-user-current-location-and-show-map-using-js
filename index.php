5f7ec1197505aa80479f74106f1f7cb0ca6a671b
CodeByVijay <gitbyvijay@gmail.com>
2023-03-02 17:07:23 +0530

    address to location map

--- index.php
+++ index.php
@@ -18,27 +18,45 @@
                 <h3 class="text-center">Get User Current Location & Show Map</h3>
             </div>
             <div class="card-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="form-group row">
+                            <div class="col-md-2"><label for="latitude">Latitude</label></div>
+                            <div class="col-md-9">
+                                <input type="text" name="latitude" class="form-control" id="latitude" placeholder="Latitude...">
+                            </div>
+                        </div>
 
-                <div class="form-group row">
-                    <div class="col-md-2"><label for="latitude">Latitude</label></div>
-                    <div class="col-md-9">
-                        <input type="text" name="latitude" class="form-control" id="latitude" placeholder="Latitude...">
+                        <div class="form-group row">
+                            <div class="col-md-2"><label for="longitude">Longitude</label></div>
+                            <div class="col-md-9">
+                                <input type="text" name="longitude" class="form-control" id="longitude" placeholder="Longitude...">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-2"><label for="address">Address</label></div>
+                            <div class="col-md-9">
+                                <input type="text" name="address" class="form-control" id="address" placeholder="address...">
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4">
+                        <div id="map" style="border: 1px solid black;">
+                            <iframe width="100%" id="iframeMap" height="170" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="">
+                            </iframe>
+                            <span id="msg" style="padding: 20px;"></span>
+                            <!-- <a href="#" onclick="getLocation()">Location Permission</a> -->
+                        </div>
                     </div>
                 </div>
 
-                <div class="form-group row">
-                    <div class="col-md-2"><label for="longitude">Longitude</label></div>
-                    <div class="col-md-9">
-                        <input type="text" name="longitude" class="form-control" id="longitude" placeholder="Longitude...">
-                    </div>
+                <!-- Address To Map View -->
+                <div style="width: 100%; border: 1px solid black; margin: 10px;" id="addressIframeDiv">
+                    <iframe width="100%" height="400" id="addressIframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=""></iframe>
                 </div>
-
-                <div id="map" style="border: 1px solid black;">
-                    <iframe width="100%" id="iframeMap" height="170" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="">
-                    </iframe>
-                </div>
-                <span id="msg"></span>
-
+                <!-- Address To Map View End -->
             </div>
         </div>
     </div>
@@ -76,6 +94,23 @@
             let url = `https://maps.google.com/maps?q=${latitude.value},${longitude.value}&output=embed`;
             iframe.attr('src', url)
         }
+
+        if ($('#address').val().length === 0) {
+            $('#addressIframeDiv').hide()
+        }
+        $(document).on('keyup', '#address', function() {
+            let val = $(this).val()
+            let div = $('#addressIframeDiv')
+            let iframe = $('#addressIframe')
+            if (val.length === 0) {
+                div.hide()
+            } else {
+                div.show()
+                let url = `https://maps.google.com/maps?q=${val}&output=embed`;
+                iframe.attr('src', url)
+            }
+
+        })
     </script>
 </body>
 
